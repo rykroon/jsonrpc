73aa0c86cc85422f9277cc22d7f4bd03641878d8
Ryan Kroon <rykroon.tech@gmail.com>
2025-12-05 11:25:55 -0500

    Added IsError() and IsSuccess() method.

--- response.go
+++ response.go
@@ -18,3 +18,11 @@ func NewSuccessResp(id *Id, result json.RawMessage) *Response {
 func NewErrorResp(id *Id, err *Error) *Response {
 	return &Response{"2.0", id, nil, err}
 }
+
+func (r *Response) IsSuccess() bool {
+	return r.Result != nil
+}
+
+func (r *Response) IsError() bool {
+	return r.Error != nil
+}
