3affa0a094e4b57107b31fc854c249d9d1c74345
Ryan Kroon <rykroon.tech@gmail.com>
2025-12-06 09:07:10 -0500

    Added helper methods for identifying the json type of a raw json message.

---
+++ json.go
@@ -0,0 +1,34 @@
+package jsonrpc
+
+import (
+	"encoding/json"
+)
+
+func isString(r json.RawMessage) bool {
+	return len(r) != 0 && r[0] == '"' && r[len(r)-1] == '"'
+}
+
+func isInt(r json.RawMessage) bool {
+	if isString(r) {
+		return false
+	}
+	var n json.Number
+	err := json.Unmarshal(r, &n)
+	if err != nil {
+		return false
+	}
+	_, err = n.Int64()
+	return err == nil
+}
+
+func isEmpty(r json.RawMessage) bool {
+	return len(r) == 0
+}
+
+func isObject(r json.RawMessage) bool {
+	return len(r) != 0 && r[0] == '{' && r[len(r)-1] == '}'
+}
+
+func isArray(r json.RawMessage) bool {
+	return len(r) != 0 && r[0] == '[' && r[len(r)-1] == ']'
+}
