bb68ee184ac62c849746b095228d5c1ebf0e0cb1
Ryan Kroon <rykroon.tech@gmail.com>
2025-12-09 19:28:24 -0500

    Added tests for response decoding.

--- response_test.go
+++ response_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestResponse(t *testing.T) {
+func TestResponseEncoding(t *testing.T) {
 	err := NewError(0, "test", nil).(*Error)
 	errWithData := NewError(0, "test", map[string]any{"foo": "bar"}).(*Error)
 
@@ -51,6 +51,55 @@ func TestResponse(t *testing.T) {
 			}
 
 			require.JSONEq(t, tc.expected, string(actual))
+		})
+	}
+}
+
+func TestResponseDecoding(t *testing.T) {
+	err := NewError(0, "test", nil).(*Error)
+	errWithData := NewError(0, "test", map[string]any{"foo": "bar"}).(*Error)
+
+	tests := []struct {
+		name       string
+		jsonString string
+		expected   *Response
+	}{
+		{
+			"err_resp_with_null_id",
+			`{"jsonrpc": "2.0", "id": null, "error": {"code": 0, "message": "test"}}`,
+			NewErrorResp(nil, err),
+		},
+		{
+			"err_resp_with_string_id",
+			`{"jsonrpc": "2.0", "id": "Hello World", "error": {"code": 0, "message": "test"}}`,
+			NewErrorResp(NewId("Hello World"), err),
+		},
+		{
+			"err_resp_with_int_id",
+			`{"jsonrpc": "2.0", "id": 123, "error": {"code": 0, "message": "test"}}`,
+			NewErrorResp(NewId(123), err),
+		},
+		{
+			"error_with_data",
+			`{"jsonrpc": "2.0", "id": 123, "error": {"code": 0, "message": "test", "data": {"foo": "bar"}}}`,
+			NewErrorResp(NewId(123), errWithData),
+		},
+		{
+			"success_resp",
+			`{"jsonrpc": "2.0", "id": "Hello World", "result": 123}`,
+			NewSuccessResp(NewId("Hello World"), json.RawMessage([]byte("123"))),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			var actual *Response
+			err := json.Unmarshal([]byte(tc.jsonString), &actual)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			require.Equal(t, tc.expected, actual)
 		})
 	}
 }
