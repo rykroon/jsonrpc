9dbb4c79954175769a910d8fd18e4c65e114012e
Ryan Kroon <rykroon.tech@gmail.com>
2026-01-16 13:45:22 -0500

    Created JsonRpcCodec

--- server.go
+++ server.go
@@ -2,32 +2,65 @@ package jsonrpc
 
 import (
 	"context"
+	"io"
 	"net/rpc"
 )
+
+// the jsonrpc codec is responsible for reading data from
+// a transport and making a jsonrpc Request
+// as well as taking a jsonrpc Response and writing it
+// to the transport
+type JsonRpcCodec interface {
+	ReadRequest(io.ReadCloser, *Request) error
+	WriteResponse(io.WriteCloser, *Response) error
+}
+
+// type JsonRpcBatchCodec interface {
+// 	ReadRequests(io.ReadCloser, []*Request) error
+// 	WriteResponses(io.WriteCloser, []*Response) error
+// }
+
+type jsonRpcCodec struct{}
+
+func (c *jsonRpcCodec) ReadRequest(r io.ReadCloser, req *Request) error {
+	defer r.Close()
+	// read body and validate jsonrpc request
+	return nil
+}
+
+func (c *jsonRpcCodec) WriteResponse(w io.WriteCloser, resp *Request) error {
+	defer w.Close()
+	// write the jsonrpc response
+	return nil
+}
+
+type JsonRpcServer2 interface {
+	ServeJsonRpc(jsonRpcCodec) error
+}
 
 type JsonRpcServer interface {
 	ServeJsonRpc(ctx context.Context, req *Request) *Response
 }
 
-type Server struct {
+type server struct {
 	server *rpc.Server
 }
 
-func NewServer() *Server {
-	return &Server{
+func NewServer() *server {
+	return &server{
 		server: rpc.NewServer(),
 	}
 }
 
-func (s *Server) Register(rcvr any) {
+func (s *server) Register(rcvr any) {
 	s.server.Register(rcvr)
 }
 
-func (s *Server) RegisterName(name string, rcvr any) {
+func (s *server) RegisterName(name string, rcvr any) {
 	s.server.RegisterName(name, rcvr)
 }
 
-func (s *Server) ServeJsonRpc(ctx context.Context, req *Request) *Response {
+func (s *server) ServeJsonRpc(ctx context.Context, req *Request) *Response {
 	codec := newCodec(req)
 	if req.IsNotification() {
 		go s.server.ServeRequest(codec)
