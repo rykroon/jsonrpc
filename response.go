b59bc83ed79342d26d098d02fdce5ba1c546dd54
Ryan Kroon <rykroon.tech@gmail.com>
2026-01-25 11:11:02 -0500

    added response interface

--- response.go
+++ response.go
@@ -1,18 +1,37 @@
 package jsonrpc
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+)
 
 type Response interface {
 	Jsonrpc() string
 	Result() any
 	Error() Error
 	Id() Id
+	json.Marshaler
+	json.Unmarshaler
 }
 
 type serverResponse struct {
 	id     Id
 	result any
 	error  Error
+}
+
+func NewErrorResponse(error Error, id Id) Response {
+	return serverResponse{
+		id:    id,
+		error: error,
+	}
+}
+
+func NewSuccessResponse(result any, id Id) Response {
+	return serverResponse{
+		id:     id,
+		result: result,
+	}
 }
 
 func (r serverResponse) Jsonrpc() string {
@@ -31,30 +50,29 @@ func (r serverResponse) Result() any {
 	return r.result
 }
 
-type serverSuccessResponse struct {
-	serverResponse
-	result any
+func (r serverResponse) MarshalJSON() ([]byte, error) {
+	temp := map[string]any{
+		"jsonrpc": r.Jsonrpc(),
+		"id":      r.Id(),
+	}
+	if r.Error() != nil {
+		temp["error"] = r.Error()
+	} else {
+		temp["result"] = r.Result()
+	}
+
+	return json.Marshal(temp)
 }
 
-func NewErrorResponse(error Error, id Id) Response {
-	return serverResponse{
-		id:    id,
-		error: error,
-	}
-}
-
-func NewSuccessResponse(result any, id Id) Response {
-	return serverResponse{
-		id:     id,
-		result: result,
-	}
+func (r serverResponse) UnmarshalJSON(b []byte) error {
+	return errors.New("not implemented")
 }
 
 type clientResponse struct {
 	RawJsonrpc json.RawMessage
 	RawResult  json.RawMessage
 	RawError   json.RawMessage
-	RawId      idBytes
+	RawId      json.RawMessage
 }
 
 func (r clientResponse) Jsonrpc() string {
@@ -66,6 +84,34 @@ func (r clientResponse) Jsonrpc() string {
 	return s
 }
 
+func (r clientResponse) Result() any {
+	if len(r.RawResult) == 0 {
+		return nil
+	}
+	var result any
+	err := json.Unmarshal(r.RawResult, &result)
+	if err != nil {
+		return nil
+	}
+	return result
+}
+
+func (r clientResponse) Error() Error {
+	if len(r.RawError) == 0 {
+		return nil
+	}
+	rpcError := clientError{}
+	err := json.Unmarshal(r.RawError, &rpcError)
+	if err != nil {
+		return nil
+	}
+	return rpcError
+}
+
 func (r clientResponse) Id() Id {
-	return r.RawId
+	if len(r.RawId) == 0 {
+		return nil
+	}
+	id := rawId(r.RawId)
+	return &id
 }
